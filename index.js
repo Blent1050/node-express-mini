0478446945e19b344acd88af91e131130bfb8b8c
Brandon <Blent1050@yahoo.com>
2019-02-11 16:49:43 -0500

    get requests set=up. Logic for post request completed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
+const express = require("express");
 
-const express = require('express');
-
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 const server = express();
 const PORT = 4000;
 
@@ -11,57 +10,49 @@ server.use(express.json());
 // ---- ROUTES ---- //
 
 //GET
-server.get('/api/users', (req, res) => {
-    db
-    .find()
+server.get("/api/users", (req, res) => {
+  db.find()
     .then(users => {
-        res.status(200).json(users)
+      res.status(200).json(users);
     })
     .catch(err => {
-        res.status(err.code).json({success: false, message: err.message})
-    })
+      res.status(500).json({ success: false, error: "The user's information could not be retrieved." });
+    });
 });
 
-server.get('/api/users/:id', (req, res) => {
-    const userId = req.params.id;
-    db
-    .findById(userId)
+server.get("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  db.findById(userId)
     .then(user => {
-        if(user){
-            res.status(200).json(user) 
-        }else{
-            res.status(404).json({success: false, message: `There is no user attached to the ID of ${userId}`})
-        }
-        
+      res.status(200).json(user);
     })
     .catch(err => {
-        res.status(err.code).json({success: false, message: err.message})
-    })
+      res.status(500).json({ success: false, error: "The user's information could not be retrieved." });
+    });
 });
 
 //POST
-server.post('/api/users', (req, res) => {
-    const user = req.body;
-    db.insert(user)
-    .then(user => {
-        res.status(201).json({success: true, user})
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  //Check if name and bio keys are attached to request. If not, return error code.
+  if(!user.name || !user.bio)
+    return res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+
+  db.insert(user)
+    .then(user => {  
+        res.status(201).json({ success: true, user: user });
     })
-    .catch(({code, message}) => {
-        res.status(code).json({success: false, message})
-    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: 'Please provide name and bio for the user.' });
+    });
 });
 
 //PUT
-server.put('/api/users/:id', (req, res) => {
-    
-});
+server.put("/api/users/:id", (req, res) => {});
 
 //DELETE
-server.delete('/api/users/:id', (req, res) => {
-    
-});
-
+server.delete("/api/users/:id", (req, res) => {});
 
 server.listen(PORT, () => {
-    console.log(`Server is listening on Port ${PORT}`)
-})
+  console.log(`Server is listening on Port ${PORT}`);
+});
