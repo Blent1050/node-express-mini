f183fa75ae556e6c8a32a00059d26487660032d9
Brandon <Blent1050@yahoo.com>
2019-02-11 15:39:52 -0500

    added basic server set-up

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require('express');
+const database = require('./data/db.js');
+const server = express();
+const PORT = 4000;
+
+server.use(express.json());
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on Port ${PORT}`)
+})
